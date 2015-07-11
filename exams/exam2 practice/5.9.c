9164a188d4edd525c93f0d8b74bb925c0b70d6c4
Steven Murr <stevemurr@stevemurrs-Mac-Pro.local>
2015-07-11 11:18:13 -0700

    updated locations

---
+++ exams/exam2 practice/5.9.c
@@ -0,0 +1,8 @@
+// 2 dollar fee 3 hours or less
+// additional .50 cents per hour after 3 hours
+// max charge - 24 hours - 10 dollars
+// display car # - hours parked - charges accrued
+// use a function called calculateCharges
+// output to tabluar format
+
+#include <stdio.h>
