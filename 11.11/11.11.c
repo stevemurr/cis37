d8448a74dbecfbe5e4cd9462a13075795811bcf3
Steven Murr <steve@stevemurr.com>
2015-07-27 11:26:26 -0700

    work work work

--- 11.11/11.11.c
+++ 11.11/11.11.c
@@ -1,170 +1,142 @@
+/*
+11.11
+Write statements that accomplish each of the following.  Assume that the structure
+
+struct person {
+  char lastName[ 15 ];
+  char firstNmae[ 15 ];
+  char age[ 4 ];
+};
+
+has been defined and that the file is already open for writing.
+
+a) Init the file "nameage.dat" with 100 records:
+lastName = "unassigned", firstName="", age=""
+b) input 10 records manually
+c) update a record
+d) delete a record
+*/
 
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-#define length_of_file 100
-#define manual_entry_length 1
+#include <stdlib.h>
 
 typedef struct {
-	size_t last_known_size;
-} size_holder;
-
-typedef struct {
-	int account_number;
-	char firstName [ 15 ];
-	char lastName [ 15 ];
-	char age [ 4 ];
+  char lastName[ 15 ];
+  char firstName[ 15 ];
+  char age[ 4 ];
 } Person;
 
-typedef struct {
-Person *array;
-size_t used;
-size_t size;
-} Array;
+typedef struct Node {
+  Person p;
+  struct Node* next;
+} Node;
 
-void initArray(Array *a, size_t initialSize) {
-a->array = (Person *)malloc(initialSize * sizeof(Person));
-a->used = 0;
-a->size = initialSize;
-}
+FILE* OpenFile( FILE *FILE , char WriteFlag ); // Get a pointer to a file
+void ReadFile( FILE *FILE , Node* Head ); // read records and return head
+void BootStrapFile( FILE *FILE ); // creates basic file for assignment
+void AddElementToList( Node* Head, Person p );
+int Length( Node* Head );
+void PrintLinkedList( Node* Head );
+void FreeLinkedList( Node** Head );
 
-void insertArray(Array *a, Person element) {
-if (a->used == a->size) {
-	a->size *= 2;
-	a->array = (Person *)realloc(a->array, a->size * sizeof(Person));
-}
-a->array[a->used++] = element;
-}
-
-void removeElement(Array *a, int index) {
-	//a->array = (Person *)realloc(a->array, a->size * sizeof(Person));
-	a->used--;
-	for (int i = index; i < a->used; i++) {
-		memcpy(&a->array[index], &a->array[index+1], sizeof(Person));
-	}
-}
-
-void freeArray(Array *a) {
-free(a->array);
-a->array = NULL;
-a->used = a->size = 0;
-}
-void read_file_into_memory(FILE *FILE, size_holder *sz, Array *person_array);
-void delete_entry(FILE *FILE, size_holder *sz, Array *person_array);
-int main(void)
+int main( void )
 {
-	// File pointer to the file
-	void create_manual_entry(FILE *FILE, Array *person_array, int size);
-	void update_entry(FILE *FILE, size_holder *sz, Array *person_array);
-	void print_contents_of_file(FILE *FILE, size_holder *sz, Array *person_array);
-	size_holder *sz;
-	FILE *file_pointer;
-	Person *person_pointer;
-	Array person_array;
-
-	// right now i'm just updating the last known size whenever i init the array.
-	initArray(&person_array, length_of_file);
-
-
-	file_pointer = fopen("nameage.dat", "w");
-	for (int i = 0; i < length_of_file; i++) {
-		Person p = {i, "unanswered", "noinput", "0"};
-		insertArray(&person_array, p);
-		fprintf(file_pointer, "%d %s %s %s\n", p.account_number,p.firstName, p.lastName, p.age);
-	}
-	sz->last_known_size = person_array.used;
-	//freeArray(&person_array);
-	fclose(file_pointer);
-	create_manual_entry(file_pointer, &person_array, sz->last_known_size);
-	print_contents_of_file(file_pointer, sz, &person_array);
-
-	//delete_entry(file_pointer, sz, &person_array);
-	//update_entry(file_pointer, &person_array);
-	//print_contents_of_file(file_pointer, &person_array);
-}
-void write_to_file(FILE *FILE, Array *person_array, char write_flag)
-{
-		FILE = fopen("nameage.dat", &write_flag);
-		//read_file_into_memory(FILE, sz, person_array);
-		for (int i = 0; i < person_array->used; i++) {
-			fprintf(FILE, "%d %s %s %s\n", person_array->array[i].account_number,
-		                                 person_array->array[i].firstName,
-																	   person_array->array[i].lastName,
-																	   person_array->array[i].age);
-		}
-}
-void print_contents_of_file(FILE *FILE, size_holder *sz, Array *person_array)
-{
-	read_file_into_memory(FILE, sz, person_array);
-	for (int i = 0; i < person_array->used; i++) {
-		printf("%d %s %s %s\n", person_array->array[i].account_number, person_array->array[i].firstName,  person_array->array[i].lastName,  person_array->array[i].age);
-	}
-	fclose(FILE);
-	//freeArray(person_array);
+  FILE *FilePointer = OpenFile(FilePointer, 'w');
+  Node* Head = malloc(sizeof(struct Node));
+  BootStrapFile(FilePointer);
+  fclose(FilePointer);
+  FilePointer = OpenFile(FilePointer, 'r');
+  ReadFile( FilePointer, Head );
+  fclose(FilePointer);
+  FilePointer = OpenFile(FilePointer, 'r');
+  ReadFile( FilePointer, Head );
+  PrintLinkedList( Head );
 }
 
-void read_file_into_memory(FILE *FILE, size_holder *sz, Array *person_array)
+void FreeLinkedList( Node** Head )
 {
-	initArray(person_array, sz->last_known_size);
-	FILE = fopen("nameage.dat", "r+");
-	int ac;
-	char first_name[50];
-	char last_name[50];
-	char age[4];
-	while (fscanf(FILE, "%d %s %s %s", &ac, first_name, last_name, age) != EOF) {
-		Person temp_person;
-		temp_person.account_number = ac;
-		strcpy(temp_person.firstName, first_name);
-		strcpy(temp_person.lastName, last_name);
-		strcpy(temp_person.age, age);
-		insertArray(person_array, temp_person);
-	}
-	sz->last_known_size = ac;
-	fclose(FILE);
-}
-void delete_entry(FILE *FILE, size_holder *sz, Array *person_array)
-{
-	int record;
-	read_file_into_memory(FILE, sz, person_array);
-	printf("%s", "Which record to delete? ");
-	scanf("%d", &record);
-	removeElement(person_array, record);
-	write_to_file(FILE, person_array, 'w');
-}
-// For appending only
-void create_manual_entry(FILE *FILE, Array *person_array, int size)
-{
-	char firstName[ 50 ];
-	char lastName[ 50 ];
-	int age = 0;
-	FILE = fopen("nameage.dat", "a");
-	for (int i = 0; i < manual_entry_length; i++) {
-		scanf("%s %s %d", firstName, lastName, &age);
-		fprintf(FILE, "%d %s %s %d", size, firstName, lastName, age);
-	}
-	fclose(FILE);
+  Node* current = *Head;
+  while ((current = *Head) != NULL ) {
+    *Head = ;
+    free(current);
+  }
+  *Head = NULL;
 }
 
-void update_entry(FILE *FILE, size_holder *sz, Array *person_array)
+void PrintLinkedList( Node* Head )
 {
-	char firstName[ 50 ];
-	char lastName[ 50 ];
-	char age[ 4 ];
-	int record;
-	read_file_into_memory(FILE, sz, person_array);
-	printf("%s", "Which record to update? ");
-	scanf("%d", &record);
-	for (int i = 0; i < person_array->used; i++) {
-		if (person_array->array[i].account_number == record) {
-			printf("%s\n", "Record found - Insert new information");
-			scanf(" %s %s %s", firstName, lastName, age);
-			strcpy(person_array->array[i].firstName, firstName);
-			strcpy(person_array->array[i].lastName, lastName);
-			strcpy(person_array->array[i].age, age);
-			fclose(FILE);
-			write_to_file(FILE, person_array, 'w');
-			return;
-		}
-	}
-	printf("%s\n", "That index doesn't exist.");
+  Node* current = Head;
+  while ( current != NULL ) {
+    printf("%s %s %s\n", current->p.lastName, current->p.firstName, current->p.age);
+    current = current->next;
+  }
+}
+
+int Length( Node* Head )
+{
+  Node* current = Head; // this is a pointer that get the pointer to
+                               // the head node.
+  int count = 0; // counter variable
+
+  while ( current != NULL ) // while current is not NULL run the loop
+  {
+    count++; // increment the count
+    current = current->next; // current gets current->next
+  }
+  return count;
+}
+
+void AddElementToList( Node* Head, Person p )
+{
+  Node* current = Head;
+  Node* newNode = malloc(sizeof( Node ));
+  strcpy(newNode->p.lastName, p.lastName);
+  strcpy(newNode->p.firstName, p.firstName);
+  strcpy(newNode->p.age, p.age);
+  newNode->next = NULL;
+  if ( current == NULL ) { // If the list is empty add this element.
+    Head = newNode;
+  } else { // Else append the element to the back of the list.
+    while ( current->next != NULL ) {
+      current = current->next;
+    }
+    current->next = newNode;
+  }
+}
+
+void BootStrapFile( FILE *FILE )
+{
+   for ( int i = 0; i < 100; i++ )
+   {
+     Person p = {"unassigned", "na", "na" };
+     fprintf( FILE, "%s %s %s\n", p.lastName, p.firstName, p.age );
+   }
+}
+
+void ReadFile( FILE *FILE, Node* Head )
+{
+  char lastName[ 15 ];
+  char firstName[ 15 ];
+  char age[ 4 ];
+  while (fscanf(FILE, "%s %s %s", lastName, firstName, age) != EOF) {
+    Person p;
+    strcpy( p.lastName, lastName );
+    strcpy( p.firstName, firstName );
+    strcpy( p.age, age );
+    AddElementToList( Head, p );
+  }
+}
+
+FILE* OpenFile( FILE *FILE , char WriteFlag )
+{
+  /*
+  Write Flags:
+  w - Open file for writing
+  a - Append to the end of the file
+  b - Write binary data to file - useful for writing structs
+  r+ - Read and append
+  */
+  FILE = fopen("nameage.dat", &WriteFlag);
+  return FILE;
 }
---
+++ 11.11/bak/11.11.c
@@ -0,0 +1,170 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define length_of_file 100
+#define manual_entry_length 1
+
+typedef struct {
+	size_t last_known_size;
+} size_holder;
+
+typedef struct {
+	int account_number;
+	char firstName [ 15 ];
+	char lastName [ 15 ];
+	char age [ 4 ];
+} Person;
+
+typedef struct {
+Person *array;
+size_t used;
+size_t size;
+} Array;
+
+void initArray(Array *a, size_t initialSize) {
+a->array = (Person *)malloc(initialSize * sizeof(Person));
+a->used = 0;
+a->size = initialSize;
+}
+
+void insertArray(Array *a, Person element) {
+if (a->used == a->size) {
+	a->size *= 2;
+	a->array = (Person *)realloc(a->array, a->size * sizeof(Person));
+}
+a->array[a->used++] = element;
+}
+
+void removeElement(Array *a, int index) {
+	//a->array = (Person *)realloc(a->array, a->size * sizeof(Person));
+	a->used--;
+	for (int i = index; i < a->used; i++) {
+		memcpy(&a->array[index], &a->array[index+1], sizeof(Person));
+	}
+}
+
+void freeArray(Array *a) {
+free(a->array);
+a->array = NULL;
+a->used = a->size = 0;
+}
+void read_file_into_memory(FILE *FILE, size_holder *sz, Array *person_array);
+void delete_entry(FILE *FILE, size_holder *sz, Array *person_array);
+int main(void)
+{
+	// File pointer to the file
+	void create_manual_entry(FILE *FILE, Array *person_array, int size);
+	void update_entry(FILE *FILE, size_holder *sz, Array *person_array);
+	void print_contents_of_file(FILE *FILE, size_holder *sz, Array *person_array);
+	size_holder *sz;
+	FILE *file_pointer;
+	Person *person_pointer;
+	Array person_array;
+
+	// right now i'm just updating the last known size whenever i init the array.
+	initArray(&person_array, length_of_file);
+
+
+	file_pointer = fopen("nameage.dat", "w");
+	for (int i = 0; i < length_of_file; i++) {
+		Person p = {i, "unanswered", "noinput", "0"};
+		insertArray(&person_array, p);
+		fprintf(file_pointer, "%d %s %s %s\n", p.account_number,p.firstName, p.lastName, p.age);
+	}
+	sz->last_known_size = person_array.used;
+	//freeArray(&person_array);
+	fclose(file_pointer);
+	create_manual_entry(file_pointer, &person_array, sz->last_known_size);
+	print_contents_of_file(file_pointer, sz, &person_array);
+
+	//delete_entry(file_pointer, sz, &person_array);
+	//update_entry(file_pointer, &person_array);
+	//print_contents_of_file(file_pointer, &person_array);
+}
+void write_to_file(FILE *FILE, Array *person_array, char write_flag)
+{
+		FILE = fopen("nameage.dat", &write_flag);
+		//read_file_into_memory(FILE, sz, person_array);
+		for (int i = 0; i < person_array->used; i++) {
+			fprintf(FILE, "%d %s %s %s\n", person_array->array[i].account_number,
+		                                 person_array->array[i].firstName,
+																	   person_array->array[i].lastName,
+																	   person_array->array[i].age);
+		}
+}
+void print_contents_of_file(FILE *FILE, size_holder *sz, Array *person_array)
+{
+	read_file_into_memory(FILE, sz, person_array);
+	for (int i = 0; i < person_array->used; i++) {
+		printf("%d %s %s %s\n", person_array->array[i].account_number, person_array->array[i].firstName,  person_array->array[i].lastName,  person_array->array[i].age);
+	}
+	fclose(FILE);
+	//freeArray(person_array);
+}
+
+void read_file_into_memory(FILE *FILE, size_holder *sz, Array *person_array)
+{
+	initArray(person_array, sz->last_known_size);
+	FILE = fopen("nameage.dat", "r+");
+	int ac;
+	char first_name[50];
+	char last_name[50];
+	char age[4];
+	while (fscanf(FILE, "%d %s %s %s", &ac, first_name, last_name, age) != EOF) {
+		Person temp_person;
+		temp_person.account_number = ac;
+		strcpy(temp_person.firstName, first_name);
+		strcpy(temp_person.lastName, last_name);
+		strcpy(temp_person.age, age);
+		insertArray(person_array, temp_person);
+	}
+	sz->last_known_size = ac;
+	fclose(FILE);
+}
+void delete_entry(FILE *FILE, size_holder *sz, Array *person_array)
+{
+	int record;
+	read_file_into_memory(FILE, sz, person_array);
+	printf("%s", "Which record to delete? ");
+	scanf("%d", &record);
+	removeElement(person_array, record);
+	write_to_file(FILE, person_array, 'w');
+}
+// For appending only
+void create_manual_entry(FILE *FILE, Array *person_array, int size)
+{
+	char firstName[ 50 ];
+	char lastName[ 50 ];
+	int age = 0;
+	FILE = fopen("nameage.dat", "a");
+	for (int i = 0; i < manual_entry_length; i++) {
+		scanf("%s %s %d", firstName, lastName, &age);
+		fprintf(FILE, "%d %s %s %d", size, firstName, lastName, age);
+	}
+	fclose(FILE);
+}
+
+void update_entry(FILE *FILE, size_holder *sz, Array *person_array)
+{
+	char firstName[ 50 ];
+	char lastName[ 50 ];
+	char age[ 4 ];
+	int record;
+	read_file_into_memory(FILE, sz, person_array);
+	printf("%s", "Which record to update? ");
+	scanf("%d", &record);
+	for (int i = 0; i < person_array->used; i++) {
+		if (person_array->array[i].account_number == record) {
+			printf("%s\n", "Record found - Insert new information");
+			scanf(" %s %s %s", firstName, lastName, age);
+			strcpy(person_array->array[i].firstName, firstName);
+			strcpy(person_array->array[i].lastName, lastName);
+			strcpy(person_array->array[i].age, age);
+			fclose(FILE);
+			write_to_file(FILE, person_array, 'w');
+			return;
+		}
+	}
+	printf("%s\n", "That index doesn't exist.");
+}
